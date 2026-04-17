c66fd8f93eb942465fa17eb257a5b55a93be09e0
Alex3glez <a5l6e7x8@gmail.com>
2026-04-17 02:36:05 +0200

    modificado a static site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export', // Habilita la generación de archivos estáticos (carpeta /out)
   images: {
+    unoptimized: true, // Obligatorio para sitios estáticos con next/image
     remotePatterns: [
       {
         protocol: "https",
